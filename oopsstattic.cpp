487601ba080517062732860467dc8b7112217cb9
anuj <anujkumarpandey2727@gmail.com>
2024-10-31 21:49:47 +0530

    oops static data member

---
+++ oopsstattic.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+
+class item{
+    static int count;
+    int number;
+    public:
+    void getdata(int a){
+        number =a ;
+        count++;
+    }
+    void getcount(){
+        cout<<" the count is : "<<count<<endl;
+    }
+};
+int item ::count;
+
+int main(){
+    item a,b,c;
+    a.getcount();
+    b.getcount();
+    c.getcount();
+    a.getdata(100);
+    a.getcount();
+    b.getdata(200);
+    b.getcount();
+    c.getdata(300);
+    c.getcount();
+    return 0;
+}
